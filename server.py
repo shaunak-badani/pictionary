644afdb1d0bd7b37c52b0d518b21fc7129cc28c2
pk13055 <pkrockstar7@gmail.com>
2019-02-13 20:24:29 +0530

    [Fixes] directory structure

---
+++ server.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python2
+from app import create_app, socketio
+from flask_cors import CORS
+app = create_app(debug=True)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+if __name__ == '__main__':
+    socketio.run(app)
