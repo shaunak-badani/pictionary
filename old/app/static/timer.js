6c0b0a486fbcd820ec60ed3b3b9f8c21a7918c0f
Shaunak Badani <shaunak.badani@research.iiit.ac.in>
2019-02-20 02:28:57 +0530

    Added Modal + tooltips

--- old/app/static/timer.js
+++ old/app/static/timer.js
@@ -1,14 +1,20 @@
 //Timer 
 
 var guessTime = 180;
+
+const start_timer = () => {
+    var tm = setInterval( () => {
+        guessTime -= 1;
+        if( guessTime <= 0 ) {
+            show_modal();
+            clearInterval(tm);
+        }
+        timer();
+    },1000);
+
+}
 				
-setInterval( () => {
-    guessTime -= 1;
-    if( guessTime <= 0 ) {
-    	leave_room();
-    }
-    timer();
-},1000);
+
 
 function timer() {
     document.querySelector('.mins').innerHTML = Math.floor(guessTime / 60);
